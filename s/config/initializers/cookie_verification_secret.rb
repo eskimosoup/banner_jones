f55951a6438d6c97a600b9700fed9a5f6a9672e7
James Doyley <jdoyley@gmail.com>
2015-09-17 16:28:52 +0100

    remove other rails folder

--- s/config/initializers/cookie_verification_secret.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random, 
-# no regular words or you'll be exposed to dictionary attacks.
-ActionController::Base.cookie_verifier_secret = '15dbd2b8e1ffbb2bf190965abbadc60ee35139112e5f960a09092192c6a53be356e15a49cc2917ff503496c9912d00d80a771be017db4b2d6c223f8724d7896c';
